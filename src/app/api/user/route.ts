6464c395fb0db6d2b324e6540aee5151e89dc36e
Chukwuka Uba <promiseuba67@gmail.com>
2024-12-15 18:48:51 +0100

    fix type errors

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getUserDetails } from "~/lib/airstack";
 
 export async function GET(request: NextRequest) {
-  const fid = request.nextUrl.searchParams.get("fid");
+  const fid = Number(request.nextUrl.searchParams.get("fid"));
   if (!fid) {
     return NextResponse.json({ error: "Fid is required" }, { status: 400 });
   }
