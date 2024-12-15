c4d8fde9f4f93f67a4648ef34bdf0611a9a99b4b
Chukwuka Uba <promiseuba67@gmail.com>
2024-12-15 12:56:16 +0100

    updated opengraph icon

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -14,9 +14,13 @@ export default async function Image() {
       <div tw="h-full w-full flex flex-col justify-center items-center relative bg-[#0F0F0F]">
         <div tw="flex items-center gap-3 mb-6">
           <div tw="w-12 h-12 rounded-full bg-purple-600 flex items-center justify-center">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="white">
-              <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
-            </svg>
+            {/* @ts-ignore Server Component */}
+            <img
+              src={new URL("./icon.png", import.meta.url).toString()}
+              width="24"
+              height="24"
+              alt="Farsight icon"
+            />
           </div>
         </div>
         <p tw="text-xl text-gray-300 mb-3">Analytics for Farcaster</p>
